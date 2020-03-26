0f2fbe5af16f1cb1586b6a41f0872de81bc58006
shivamsahay0 <60652255+shivamsahay0@users.noreply.github.com>
2020-03-27 00:00:45 +0530

    Selenium-Testing-1

---
+++ Q5.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","/home/shivam/Desktop/Chrome Driver/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://www.tothenew.com");
+	    driver.findElement(By.xpath("(//a[@id='h-contact-us'])[2]")).click();
+	    System.out.println(driver.getTitle());
+	}
+
+}
